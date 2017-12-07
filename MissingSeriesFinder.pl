bcedb279e6a935decbf9cd646154caa6697d1437
necrodraco <ygohameln@gmail.com>
2017-12-07 13:02:57 +0100

    Improving Speed. Update to 3.0.1

--- MissingSeriesFinder.pl
+++ MissingSeriesFinder.pl
@@ -1,15 +1,14 @@
 #!/usr/bin/perl
 
-use Data::Dumper; 
 use Getopt::Long;
-
-use lib 'lib';
-use SqlManager; 
-use XmlReader; 
 use IO::Handle; 
 use File::Copy "cp"; 
-use File::Basename;
-use File::Path qw/make_path/;
+use File::Path "make_path";
+use XML::Simple; 
+use WWW::Mechanize;
+use DBI; 
+
+use POSIX "strftime";
 
 my $path   = '';
 GetOptions (
@@ -17,64 +16,89 @@ GetOptions (
 ) or die("Error in command line arguments\n");
 
 if($path ne '' && -e $path ){
-	print 'path is '.(Dumper $path);
-
-	my $xmlReader = XmlReader->new();
-
-	my $source = $xmlReader->read($path.'settings.xml')->{'setting'};
+	my $source = XMLin($path.'settings.xml')->{'setting'};
 	
 	open OUTPUT, '>', $source->{'Path_to_Log'}->{'value'}.'missing.log' or die $!;
 	STDOUT->fdopen( \*OUTPUT, 'w' ) or die $!;
 
-	my $sql; 
-	if($source->{'sql'}->{'value'} == 0){
-		$sql = SqlManager->new(
-			'host' => $source->{'host'}->{'value'}, 
-			'port' => $source->{'port'}->{'value'}, 
-			'dbname' => $source->{'dbName'}->{'value'}, 
-			'user' => $source->{'username'}->{'value'}, 
-			'pass' => $source->{'password'}->{'value'}, 
-		);
-		$sql->connect();
-	}
-	my $list = $sql->getSeries();
-	
-	my $list2 = ();
-	while(my ($id, $serie) = each %{$list}){
-		my $content = $xmlReader->readFromXml('http://www.thetvdb.com/api/1D62F2F90030C444/series/'.$id.'/all/en.xml');
-		$list2->{$id} = $content->{$id};
-	}
-
-	my $missing = (); 
-	while(my ($id2, $serie2) = each %{$list2}){
-		my $episodes = $list->{$id2}->{'episodes'};
-		my $episodes2 = $serie2->{'episodes'}; 
-		while(my ($episode, $stat) = each %{$episodes2}){
-			if(!defined($episodes->{$episode})){
-				$missing->{$serie2->{'name'}}->{$episode} = 1; 
-			}
-		}
-	}
-
+	print 'start: '.getTime();
 	my $log = 0;
-	if($source->{'LogOption'}->{'value'} eq 'true'){
-		$log = 1; 
+	$log = 1 if($source->{'LogOption'}->{'value'} eq 'true');
+
+	my $dbh; 
+	if($source->{'sql'}->{'value'} == 0){
+		
+		my (
+			$db_host,$db_port, $db_name
+		) = (
+			$source->{'host'}->{'value'}, 
+			$source->{'port'}->{'value'}, 
+			$source->{'dbName'}->{'value'}
+		);
+		
+		$dbh = DBI->connect("DBI:mysql:database=$db_name;host=$db_host;port=$db_port", $source->{'username'}->{'value'}, $source->{'password'}->{'value'});
 	}
 
-	while(my ($name, $episodes) = each %{$missing}){
-		print "$name\n"; 
-		while(my ($ename, $stat) = each %{$episodes}){
-			print "\t$ename\n";
-			if($log == 1){
-				my $dir = $source->{'Path_to_Log'}->{'value'}."missing/$name/";
-				my $file = $dir."$ename.disc"; 
-				make_path($dir) if( !(-e $dir));
-				cp('./resources/empty.disc', $file) or die 'copy Failed';
-			};
+	my $sth = $dbh->prepare('
+		SELECT 
+			t.c00 as seriesName, t.c12 as seriesId, 
+		    e.c12 as seasonNumber, e.c13 as episodeNumber
+		FROM 
+			tvshow t
+		JOIN 
+			episode e 
+		ON 
+			t.idShow = e.idShow'
+	);
+	my $worked = $sth->execute();
+	
+	my %list; 
+	if(defined($worked) && $worked ne '0E0'){
+		while(my $row = $sth->fetchrow_hashref()){
+			$list{$row->{'seriesId'}}{'name'} = $row->{'seriesName'} if(!defined($list{$row->{'seriesId'}}{'name'}));
+			$list{$row->{'seriesId'}}{'episodes'}{'S'.setDigit($row->{'seasonNumber'}).'E'.setDigit($row->{'episodeNumber'})} = 1;
 		}
-		print "\n";
 	}
-
+	
+	my $urlReader = WWW::Mechanize->new(); 
+	my $found = ''; 
+	
+	while(my ($id, $serie) = each %list){
+		my $url = "http://www.thetvdb.com/api/1D62F2F90030C444/series/$id/all/en.xml"; 
+		eval{
+			$found = XMLin($urlReader->get($url)->decoded_content());
+		};
+		if($@){
+			print "Can't connect to Url $url. Series Name is $serie->{'name'}\n";
+			next; 
+		}
+		
+		my $dir = $source->{'Path_to_Log'}->{'value'}."missing/$serie->{'name'}/";
+		foreach my $episode(values %{$found->{'Episode'}}){
+			my $ename = 'S'.setDigit($episode->{'SeasonNumber'}).'E'.setDigit($episode->{'EpisodeNumber'}); 
+			next if(defined($list{$id}{'episodes'}{$ename}));
+			print "$serie->{'name'}: $ename\n";
+			if($log == 1){
+				make_path($dir) if( !(-e $dir));
+				cp('./resources/empty.disc', $dir."$ename.disc") or die 'copy Failed';
+			};
+			
+		}
+	}
 }else{
 	print 'Arguments are not set or false. Please run directly from Kodi Module';
+}
+
+print 'finish: '.getTime();
+
+sub setDigit(){
+	my ($number) = @_;
+	if(length($number) < 2){
+		$number = '0'.$number; 
+	}
+	return $number; 
+}
+
+sub getTime(){
+	return strftime "%H:%M:%S \n", localtime(time); 
 }
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # MissingSeriesFinder
 
-An KODI-Module to Check your Library for Missing Episodes. 
+Do you think your Series got new Episodes or Specials, and you don't recognize it? Then use this KODI-Module to get an Log of the missing Episodes. 
 
-You need Perl installed. It's default on every Linux System
+You need Perl installed. It's default on every Linux System. 
 
-#In Development
+Supports only MySql-Databases at the moment
